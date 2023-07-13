500887cac3d61673e00da98a126d35b22d167ff2
Contineo <marinzilic507@gmail.com>
2023-07-13 16:18:03 +0200

    carouselv2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,24 +26,21 @@
     </div>
     <div class="carousel-inner ">
       <div class="carousel-item active ">
-        <img  class="d-block movieBackground" alt="..."/>
+        <img :src="moviePosterBackground" class="d-block movieBackground" alt="..."/>
         <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+          <h2>{{ titlePoster }}</h2>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="" class="d-block movieBackground" alt="..." />
+        <img :src="moviePosterBackground2" class="d-block movieBackground" alt="..." />
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
+          <h2>{{ titlePoster2 }}</h2>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="" class="d-block  movieBackground" alt="..." />
+        <img :src="moviePosterBackground3" class="d-block  movieBackground" alt="..." />
         <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+          <h2>{{ titlePoster3 }}</h2>
         </div>
       </div>
     </div>
@@ -69,17 +66,32 @@
 </template>
 <script>
 import axios from "axios";
+
 export default {
   data() {
     return {
       selectedMovie: null,
       movieTitle: [],
       moviePosters:[],
-     
-
+      randomNumber:null,
+      moviePosterBackground:null,
+      moviePosterBackground2:null,
+      moviePosterBackground3:null,
+      moviePoster:null,
+      moviePoster2: null,
+      titlePoster:'',
+      titlePoster2:'',
+      titlePoster3:'',
+      
     };
   },
   created() {
+    this.randomNumber =  Math.floor(Math.random() * 20);
+    console.log(this.randomNumber)
+    this.randomNumber2 =  Math.floor(Math.random() * 20);
+    console.log(this.randomNumber2)
+    this.randomNumber3 =  Math.floor(Math.random() * 20);
+    
     axios
       .get(
         "https://api.themoviedb.org/3/movie/popular?api_key=2b24ba56d7cced960b52aa5d062f497e&language=en-US"
@@ -95,9 +107,27 @@ export default {
           this.movieTitle.push(element.title);
           this.moviePosters.push(element.backdrop_path)
           console.log(this.moviePosters)
-          this.moviePoster = this.selectedMovie[1].backdrop_path
-         
-          /* this.moviePosterBackground = `https://image.tmdb.org/t/p/original${this.moviePoster}`;  */
+
+          this.moviePoster = this.selectedMovie[this.randomNumber].backdrop_path
+          this.moviePosterBackground = `https://image.tmdb.org/t/p/original${this.moviePoster}`;
+          this.moviePoster= this.selectedMovie[this.randomNumber];
+          this.titlePoster = this.moviePoster.title
+       
+
+          this.moviePoster2 = this.selectedMovie[this.randomNumber2].backdrop_path
+          this.titlePoster2 = this.moviePoster2.title
+          this.moviePosterBackground2 = `https://image.tmdb.org/t/p/original${this.moviePoster2}`;
+          this.moviePoster2= this.selectedMovie[this.randomNumber2];
+          this.titlePoster2 = this.moviePoster2.title
+
+
+          this.moviePoster3 = this.selectedMovie[this.randomNumber3].backdrop_path
+          this.titlePoster3 = this.moviePoster3.title
+          this.moviePosterBackground3 = `https://image.tmdb.org/t/p/original${this.moviePoster3}`;
+          this.moviePoster3= this.selectedMovie[this.randomNumber3];
+          this.titlePoster3 = this.moviePoster3.title
+           
+
          
         }
       });
@@ -111,7 +141,10 @@ export default {
         height: 500px;
        position: relative;
        left:23%;
+       
     }
+
+    
 
    
     
