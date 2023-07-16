ad81dfb15520a2bb533f1da7be30a6d0f6b36885
Contineo <marinzilic507@gmail.com>
2023-07-16 14:35:32 +0200

    search za mobilne uredjaje

--- src/App.vue
+++ src/App.vue
@@ -40,20 +40,65 @@ export default {
     },
   },
   watch: {
-  searchQuery(newValue) {
-    if (newValue === "") {
-      this.resetSearchResults();
-    }
-    this.debouncedPerformSearch();
+    searchQuery(newValue) {
+      if (newValue === "") {
+        this.resetSearchResults();
+      }
+      this.debouncedPerformSearch();
+    },
   },
-},
 };
+
+/* $(document).ready(function() {
+  $('.searchBarr').hide();
+  $('.mobileSearch').click(function() {
+    $('.searchBarr').fadeOut(300).animate({ width: 'toggle' }, 300);
+  });
+}); */
+
+$(document).ready(function() {
+  $('.searchBarr').hide();
+  $('.mobileSearch').click(function() {
+    var searchBarr = $('.searchBarr');
+    
+    if (searchBarr.is(':visible')) {
+      searchBarr.fadeTo(300).animate({ width: 'toggle' }, 400)
+    } else {
+      searchBarr.fadeOut(300).animate({ width: 'toggle' }, 400)
+    }
+  });
+});
 </script>
 <template>
   <header>
     <nav class="navbar navbar-expand-lg bgNav">
       <div class="container-fluid">
         <img src="./images/logo.png" alt="" />
+        <div class="divProblem">
+          <form class="d-flex" role="search" @submit.prevent>
+            <input
+              class="form-control me-2 searchBarr"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              @input="debouncedPerformSearch"
+              v-model="searchQuery"
+            />
+          </form>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="23"
+            height="23"
+            fill="currentColor"
+            class="bi bi-search mobileSearch"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+            />
+          </svg>
+        </div>
+
         <button
           class="navbar-toggler"
           type="button"
@@ -134,6 +179,9 @@ img {
   height: 40px;
 }
 
+.mobileSearch{
+  display: none;
+}
 .searchBar {
   width: 230px;
   height: 40px;
@@ -185,6 +233,44 @@ img {
   .SVG {
     position: relative;
     top: 2rem;
+    opacity: 0;
+  }
+
+  .searchBar {
+    opacity: 0;
+  }
+
+  .searchBarr {
+    position: relative;
+    left:30px;
+    width: 230px;
+    height: 30px;
+    border-radius: 30px;
+    outline: none;
+    border: none;
+    background-color: #202020;
+    color: #fff;
+    
+  }
+
+  .searchBarr::placeholder{
+    color:#fff;
+  }
+
+  .mobileSearch {
+    display: block;
+    color: #fff;
+    position: relative;
+    
+  }
+
+  .divProblem{
+  
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    width:65%;
+    
   }
 }
 </style>
